bc907ed6393632a9d98d94325143ac87768671fd
Killian Godet <godetkillian@yahoo.com>
2022-08-26 16:48:03 +0200

    Résolution bug affichage slots

---
+++ public/assets/js/api.js
@@ -0,0 +1,23 @@
+
+
+const url = "/api";
+
+const apiGetAllSlots = (onSuccess) => {
+    const request = new XMLHttpRequest();
+    let url = 'https://localhost:8000/api'
+    request.open("GET", url+"/weekslots", true);
+    request.addEventListener("readystatechange", function (){
+        if(request.readyState === XMLHttpRequest.DONE){
+            let response = request;
+            if(request.status === 200){
+                let res = JSON.parse(request.responseText);
+                setTimeout(onSuccess(res), 5000);
+            } else if(request.status === 400){
+                console.error("Une erreur s'est produite : ", response.status);
+            } else {
+                console.error("Une erreur s'est produite : ", response.status);
+            }
+        }
+    })
+    request.send();
+}
