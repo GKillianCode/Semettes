3cd701704dee18c151f28014da8980c6a7550a87
Killian Godet <godetkillian@yahoo.com>
2022-09-02 11:32:16 +0200

    Ajout de la fonction update user data

--- src/Controller/DashboardAdminController.php
+++ src/Controller/DashboardAdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\BookingRepository;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -53,16 +54,29 @@ class DashboardAdminController extends AbstractController
         return $apiResponse;
     }
 
-    // #[IsGranted('ROLE_ADMIN')]
-    // #[Route('/admindashboard/{id}/updatebooking/{bookingid}', name: 'app_dashboard_update')]
-    // public function bookingUpdate(
-    //     int $id,
-    //     int $bookingid,
-    //     BookingRepository $bookingRepository
-    // ): Response
-    // {
-    //     $booking = $bookingRepository->findOneById($bookingid);
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/admindashboard/{id}/updatebooking/{bookingid}', name: 'app_dashboard_update')]
+    public function bookingUpdate(
+        int $id,
+        int $bookingid,
+        BookingRepository $bookingRepository,
+        EntityManager $em
+    ): Response
+    {
+        $firstname = htmlentities($_POST['firstname']);
+        $lastname = htmlentities($_POST['lastname']);
+        $phone = htmlentities($_POST['phone']);
+        $email = htmlentities($_POST['email']);
 
-    //     return $this->redirectToRoute('app_dashboard_admin', ['id' => $id]);
-    // }
+        $booking = $bookingRepository->findOneById($bookingid);
+        $booking->setFirstname = $firstname;
+        $booking->setLastname = $lastname;
+        $booking->setPhone = $phone;
+        $booking->setEmail = $email;
+
+        $em->persist($booking);
+        $em->flush($booking);
+
+        return $this->redirectToRoute('app_dashboard_admin', ['id' => $id]);
+    }
 }
